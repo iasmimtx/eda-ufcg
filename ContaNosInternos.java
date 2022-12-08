1301bb7f46e472c4b8d3678a6ca397dc990e916a
Iasmim Torres <iasmimt31@gmail.com>
2022-12-08 19:45:35 -0300

    questoes

---
+++ ContaNosInternos.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class ContaNosInternos {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] valores = arrInteiros(sc);
+        MyBST arvore = new MyBST();
+        for (int valor : valores) {
+            arvore.addRecursive(valor);
+        }
+        System.out.println(arvore.contaNo());
+    }
+
+    private static int[] arrInteiros(Scanner sc) {
+        String[] entrada = sc.nextLine().split(" ");
+        int[] saida = new int[entrada.length];
+        for (int i = 0; i < entrada.length; i++) {
+            saida[i] = Integer.parseInt(entrada[i]);
+        }
+        return saida;
+    }
+
+}
+
+class MyBST {
+    private Nodeh raiz;
+
+    public boolean isEmpty() {
+        return raiz == null;
+    }
+
+    public void addRecursive(int elemento) {
+        if (isEmpty()) {
+            this.raiz = new Nodeh(elemento);
+        } else {
+            addRecursive(this.raiz, elemento);
+        }
+    }
+
+    private void addRecursive(Nodeh node, int elemento) {
+        if (elemento < node.value) {
+            if (node.left == null) {
+                Nodeh newNode = new Nodeh(elemento);
+                node.left = newNode;
+                newNode.parent = node;
+                return;
+            }
+            addRecursive(node.left, elemento);
+        } else {
+            if (node.right == null) {
+                Nodeh newNo = new Nodeh(elemento);
+                node.right = newNo;
+                newNo.parent = node;
+                return;
+            }
+            addRecursive(node.right, elemento);
+        }
+    }
+
+    public int contaNo() {
+        return contaNo(raiz);
+    }
+
+    private int contaNo(Nodeh node) {
+        int resul = 0;
+        if (node != null && !node.isLeaf()) {
+            resul = 1 + contaNo(node.left) + contaNo(node.right);
+        }
+        return resul;
+    }
+}
+
+class Nodeh {
+
+    int value;
+    Nodeh left, right, parent;
+
+    public Nodeh(int v) {
+        this.value = v;
+    }
+
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+
+
+}
--- EncaminhamentoArvoreBinaria.java
+++ EncaminhamentoArvoreBinaria.java
@@ -4,8 +4,14 @@ public class EncaminhamentoArvoreBinaria {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] valores = arrInteiros(sc);
+        BST arvore = new BST();
+        for (int valor : valores) {
+            arvore.addRecursive(valor);
+        }
 
-
+        System.out.println(arvore.preOrder());
+        System.out.println(arvore.order());
+        System.out.println(arvore.posOrder());
     }
 
     private static int[] arrInteiros(Scanner sc) {
@@ -14,39 +20,109 @@ public class EncaminhamentoArvoreBinaria {
         for (int i = 0; i < entrada.length; i++) {
             saida[i] = Integer.parseInt(entrada[i]);
         }
-        return  saida;
+        return saida;
     }
-
-
 }
 
-class BST{
+class BST {
     private Nod3 raiz;
-
 
     public boolean isEmpty() {
         return this.raiz == null;
     }
 
-    public void addRecusrive(int elemento){
-        if(isEmpty()){
+    public void addRecursive(int elemento) {
+        if (isEmpty()) {
             this.raiz = new Nod3(elemento);
         } else {
-            addRecusrive(this.raiz, elemento);
+            addRecursive(this.raiz, elemento);
         }
     }
 
-    private void addRecusrive(Nod3 raiz, int elemento) {
+    private void addRecursive(Nod3 node, int elemento) {
+        if (elemento < node.value) {
+            if (node.left == null) {
+                Nod3 newNode = new Nod3(elemento);
+                node.left = newNode;
+                newNode.parent = node;
+                return;
+            }
+            addRecursive(node.left, elemento);
+        } else {
+            if (node.right == null) {
+                Nod3 newNo = new Nod3(elemento);
+                node.right = newNo;
+                newNo.parent = node;
+                return;
+            }
+            addRecursive(node.right, elemento);
+        }
+    }
 
+
+    public String preOrder() {
+        return "[" + preOrder(raiz) + "]";
+
+    }
+
+    private String preOrder(Nod3 raiz) {
+        String resultado = "";
+        if (raiz != null) {
+            resultado += raiz.value;
+            if (raiz.left != null) {
+                resultado +=  ", "  + preOrder(raiz.left) ;
+            }
+            if (raiz.right != null) {
+                resultado += ", "  + preOrder(raiz.right);
+            }
+        }
+        return resultado;
+    }
+
+    public String order() {
+        return "[" + order(raiz) + "]";
+
+    }
+
+    private String order(Nod3 raiz) {
+        String resultado = "";
+        if (raiz != null) {
+            if (raiz.left != null) {
+                resultado += order(raiz.left) + ", "  ;
+            }
+            resultado += raiz.value;
+            if (raiz.right != null) {
+                resultado += ", "  + order(raiz.right);
+            }
+        }
+        return resultado;
+    }
+
+    public String posOrder() {
+        return "[" + posOrder(raiz) + "]";
+
+    }
+
+    private String posOrder(Nod3 raiz) {
+        String resultado = "";
+        if (raiz != null) {
+            if (raiz.left != null) {
+                resultado += posOrder(raiz.left) + ", "  ;
+            }
+            if (raiz.right != null) {
+                resultado +=  posOrder(raiz.right) + ", ";
+            }
+            resultado += raiz.value;
+        }
+        return resultado;
     }
 
 }
 
-
 class Nod3 {
 
     int value;
-    Nod3 lef, right, parent;
+    Nod3 left, right, parent;
 
     public Nod3(int v) {
         this.value = v;
